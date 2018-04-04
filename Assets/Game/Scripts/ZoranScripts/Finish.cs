12f863f268ba44c1baae3720786c3c3feb02207c
Unknown <zster1204@gmail.com>
2018-04-04 09:17:55 +0200

    Finish

---
+++ Assets/Game/Scripts/ZoranScripts/Finish.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Finish : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider Player)
+    {
+        if (Player.gameObject.tag == "Player")
+        {
+            Application.Quit();
+        }
+    }
+}
