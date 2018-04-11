8dae3f6f5754bef1523f6e4552505287e4b96e65
Unknown <zster1204@gmail.com>
2018-04-11 10:43:55 +0200

    Finish Script

--- Assets/Game/Scripts/ZoranScripts/Finish.cs
+++ Assets/Game/Scripts/ZoranScripts/Finish.cs
@@ -1,14 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Finish : MonoBehaviour {
+
+    public GameObject gameOverPanel;
+
+    public Text gameOverText;
+
+    void Start()
+    {
+        gameOverPanel.SetActive(false);
+    }
 
     private void OnTriggerEnter(Collider Player)
     {
         if (Player.gameObject.tag == "Player")
         {
-            Application.Quit();
+            gameOverPanel.SetActive(true);
+            gameOverText.text = Player.gameObject.name + " wins!";
         }
     }
 }
